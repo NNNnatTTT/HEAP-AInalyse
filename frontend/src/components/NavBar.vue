9a542a4d05faf2a5e7bc9f15d9f42bc3c58429df
Kenneth Tay <kenneth21tay@gmail.com>
2025-06-13 16:03:09 +0800

    Changed title and name of app.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-    <div class="text-xl font-bold text-gray-800">MyApp</div>
+    <div class="text-xl font-bold text-gray-800">AInalyse</div>
     <ul class="flex space-x-6">
       <li><router-link to="/" class="text-gray-700 hover:text-blue-600">Home</router-link></li>
       <li><router-link to="/about" class="text-gray-700 hover:text-blue-600">About</router-link></li>
